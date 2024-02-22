2a7843dd8f005a6f253f05261232127006db2167
kuchbhimingwal <shubhammingi@gmail.com>
2024-02-22 06:49:14 -0500

    created Form to write the posts

--- 03megaBlog/src/components/index.js
+++ 03megaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './Container/Container';
 import Logo from "./Logo";
 import LogoutButton from './Header/LogoutButton'
+import RTE from "./RTE";
+import Signup from "../pages/Signup";
 
-export {Header, Footer, Container, Logo, LogoutButton}
+export {Header, Footer, Container, Logo, LogoutButton, RTE,Signup}
