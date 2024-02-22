5cfe20ddfd74880714bb252ead2d2b7e21424225
kuchbhimingwal <shubhammingi@gmail.com>
2024-02-22 08:01:39 -0500

    added all the pages and finished the routing next is just the debuging left

--- 03megaBlog/src/components/index.js
+++ 03megaBlog/src/components/index.js
@@ -5,5 +5,10 @@ import Logo from "./Logo";
 import LogoutButton from './Header/LogoutButton'
 import RTE from "./RTE";
 import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+// import Post from './Post'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
-export {Header, Footer, Container, Logo, LogoutButton, RTE,Signup}
+export {Header, Footer, Container, Logo, LogoutButton, RTE, Signup, Login, PostForm, PostCard, AuthLayout}
