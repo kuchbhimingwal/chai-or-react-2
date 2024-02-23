6b3d9a1d619584386c66f34f7aae43cff6c5b48f
kuchbhimingwal <shubhammingi@gmail.com>
2024-02-23 09:06:04 -0500

    didi some final debugging just left with some todos for later

--- 03megaBlog/src/pages/Post.jsx
+++ 03megaBlog/src/pages/Post.jsx
@@ -11,8 +11,7 @@ export default function Post() {
     const navigate = useNavigate();
 
     const userData = useSelector((state) => state.auth.userData);
-
-    const isAuthor = post && userData ? post.userId === userData.$id : false;
+    const isAuthor = post && userData ? post.userid === userData.$id : false;
 
     useEffect(() => {
         if (slug) {
